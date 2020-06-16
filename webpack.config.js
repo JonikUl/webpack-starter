1532e27ef9f6eae8ee3788ade05e7a2f9d1c8c36
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2020-06-16 21:23:33 +0400

    add clean-webpack-plugin
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
+//Core
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
+// the path(s) that should be cleaned
+const pathToClean = ['dist'];
+
+//the clean options to use
+const cleanOptions = {
+  verbose: true,
+}
+
+/**
+ * Config types
+ * Function
+ * Object
+ * Promise
+ */
 module.exports = () => {
   return {
     mode: 'none',
     devtool: false,
     plugins: [
+      //Every plugin is constructor
       new HtmlWebpackPlugin({
         template: './static/template.html',
         title: 'Webpack',
         /* favicon: './static/favicon.ico', */
       }),
+      new CleanWebpackPlugin(pathToClean, cleanOptions),
     ]
   }
 }
