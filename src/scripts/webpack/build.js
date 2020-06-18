bc6e4dcfab504c184a4b875dfb9e710971ca4cbc
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2020-06-18 20:21:31 +0400

    add node api build script
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- src/scripts/webpack/build.js
+++ src/scripts/webpack/build.js
@@ -1,1 +1,41 @@
-console.log('Build!');
+//Core
+const webpack = require('webpack');
+const chalk = require('chalk');
+
+//Config
+const getConfig = require('./webpack.config.js');
+
+//Compiler Webpack
+const compiler = webpack(getConfig());
+
+//Run
+compiler.run((error, stats) => {
+  if (error) {
+    console.error(error.stack || error);
+
+    if (error.details) {
+      console.error(error.details);
+    }
+    return null;
+  }
+
+  const info = stats.toString({
+    hash: true,
+    version: true,
+    colors: true,
+    modules: false,
+    entrypoints: false,
+  });
+
+  console.log(chalk.greenBright('Build completed'));
+  console.log(info);
+
+  if (stats.hasErrors()) {
+    console.log(chalk.redBright('Error!'));
+    console.error(info);
+  }
+  if (stats.hasWarnings()) {
+    console.log(chalk.yellowBright('Warning!'));
+    console.warn(info);
+  }
+});
